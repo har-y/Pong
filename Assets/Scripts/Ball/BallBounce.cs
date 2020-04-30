4c5dd9d0552db9654c4987576f3d98a94410b1af
har-y <jakub.harenda@gmail.com>
2020-05-01 00:42:11 +0200

    Add: Ball - Ball Bounce Script Behavior

--- Assets/Scripts/Ball/BallBounce.cs
+++ Assets/Scripts/Ball/BallBounce.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class BallBounce : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    private float _moveSpeed;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _moveSpeed = GetComponent<BallMovement>().moveSpeed;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,29 @@ public class BallBounce : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player 1")
+        {
+            float y = HitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+            Vector2 direction = new Vector2(1, y).normalized;
+
+            _rb.velocity = direction * _moveSpeed;
+        }
+
+        if (collision.gameObject.tag == "Player 2")
+        {
+            float y = HitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
+            Vector2 direction = new Vector2(-1, y).normalized;
+
+            _rb.velocity = direction * _moveSpeed;
+        }
+    }
+
+    private float HitFactor(Vector2 ballPosition, Vector2 playerPosition, float playerHeight)
+    {
+        return (ballPosition.y - playerPosition.y) / playerHeight;
     }
 }
