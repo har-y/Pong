2b60afc8e24573633281aa50dd730605c4ad1018
har-y <jakub.harenda@gmail.com>
2020-05-01 00:05:15 +0200

    Add: Ball - Ball Movement Script Behavior

--- Assets/Scripts/Ball/BallMovement.cs
+++ Assets/Scripts/Ball/BallMovement.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    public float moveSpeed = 10f;
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb.velocity = Vector2.right * moveSpeed;
     }
 
     // Update is called once per frame
